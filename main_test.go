7e10f4d571e9e7bf02c81769ca579ab3605fc8be
sy <orzsxc@163.com>
2025-03-23 22:12:56 +0800

    1st commit

---
+++ main_test.go
@@ -0,0 +1,13 @@
+package main
+import (
+  "os"
+  "testing"
+)
+func TestMain(m *testing.M) {
+  os.Exit(m.Run())
+}
+func TestHelloWorld(t *testing.T) {
+  if HelloWorld()!= "Hello World!" {
+    t.Errorf("got %s expected %s", HelloWorld(), "Hello World!")
+  }
+}
